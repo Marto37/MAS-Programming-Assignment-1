a620d39bd41549ade74951911631c4fcb3f62a11
malekamiri <amirimalek@gatech.edu>
2020-08-31 18:53:34 -0400

    created project

---
+++ Chat-App-MAS/Chat-App-MAS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat-App-MAS
+//
+//  Created by Malek Amiri on 8/31/20.
+//  Copyright © 2020 Malek Amiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
