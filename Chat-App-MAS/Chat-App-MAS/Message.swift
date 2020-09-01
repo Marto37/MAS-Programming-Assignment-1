d0c65f5c448c68bb54f8c36d0b257d9b46af84fd
Marto37 <mcosta33@gatech.edu>
2020-08-31 20:20:10 -0400

    basic Message struct

---
+++ Chat-App-MAS/Chat-App-MAS/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Chat-App-MAS
+//
+//  Created by Marcelo Costa on 8/31/20.
+//  Copyright © 2020 Malek Amiri. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let content: String
+    let sender: String
+    let timestamp: String
+}
